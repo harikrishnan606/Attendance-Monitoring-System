1a15308063d9ca84c364987372c233abba69f4f6
Hari <harikrishnan606@gmail.com>
2018-02-23 13:33:21 -0500

    camera check added

--- test.py
+++ test.py
@@ -12,27 +12,36 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 #font = cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 1, 1)
 while True:
     ret, im =cam.read()
-    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-    faces=faceCascade.detectMultiScale(gray, 1.2,5)
-    for(x,y,w,h) in faces:
-        cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
-        Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
-        if(conf<50):
-            if(Id==1):
-                Id="HARI"
-            elif(Id==2):
-                Id="2"
-            elif(Id==3):
-                Id="3"
-            elif(Id==4):
-                Id="4"
-
-        else:
-            Id="Unknown"
-        #cv2.cv.PutText(cv2.cv.fromarray(im),str(Id), (x,y+h),font, 255)
-        cv2.putText(im,str(Id), (x,y+h),font,1,(255, 255,255),2, cv2.LINE_AA)
-    cv2.imshow('im',im) 
-    if cv2.waitKey(10) & 0xFF==ord('q'):
-        break
+    if(ret == True):
+		gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+		faces=faceCascade.detectMultiScale(gray, 1.2,5)
+		for(x,y,w,h) in faces:
+			cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
+			Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
+			if(conf<50):
+				if(Id==1):
+					Id="1"
+				elif(Id==2):
+					Id="2"
+				elif(Id==3):
+					Id="3"
+				elif(Id==4):
+					Id="4"
+				elif(Id==5):
+					Id="5"
+				elif(Id==6):
+					Id="6"
+				elif(Id==7):
+					Id="7"
+			else:
+				Id="Unknown"
+			#cv2.cv.PutText(cv2.cv.fromarray(im),str(Id), (x,y+h),font, 255)
+			cv2.putText(im,str(Id), (x,y+h),font,1,(255, 255,255),2, cv2.LINE_AA)
+		cv2.imshow('im',im) 
+		if cv2.waitKey(10) & 0xFF==ord('q'):
+			break
+    else:
+		print("camera not detected")
+		break
 cam.release()
 cv2.destroyAllWindows()
