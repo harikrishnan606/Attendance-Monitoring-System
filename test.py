3bed75a52e468888d14f62168965ea1773e5c6fc
Hari <harikrishnan606@gmail.com>
2018-02-23 15:47:50 -0500

    array,update db,functional,keypress function

--- test.py
+++ test.py
@@ -1,47 +1,75 @@
 import cv2
 import numpy as np
-
+import requests
+import time
 recognizer = cv2.face.createLBPHFaceRecognizer()
 recognizer.load('trainner/trainner.yml')
 cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath);
+attendarr = [0,0,0,0,0,0,0,0,0,0]
 
 
-cam = cv2.VideoCapture(0)
-font = cv2.FONT_HERSHEY_SIMPLEX
-#font = cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 1, 1)
-while True:
-    ret, im =cam.read()
-    if(ret == True):
-		gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-		faces=faceCascade.detectMultiScale(gray, 1.2,5)
-		for(x,y,w,h) in faces:
-			cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
-			Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
-			if(conf<50):
-				if(Id==1):
-					Id="1"
-				elif(Id==2):
-					Id="2"
-				elif(Id==3):
-					Id="3"
-				elif(Id==4):
-					Id="4"
-				elif(Id==5):
-					Id="5"
-				elif(Id==6):
-					Id="6"
-				elif(Id==7):
-					Id="7"
-			else:
-				Id="Unknown"
-			#cv2.cv.PutText(cv2.cv.fromarray(im),str(Id), (x,y+h),font, 255)
-			cv2.putText(im,str(Id), (x,y+h),font,1,(255, 255,255),2, cv2.LINE_AA)
-		cv2.imshow('im',im) 
-		if cv2.waitKey(10) & 0xFF==ord('q'):
-			break
-    else:
-		print("camera not detected")
-		break
-cam.release()
-cv2.destroyAllWindows()
+r = requests.get("http://localhost/attendance")
+print(r.content)
+def keydetect():
+	print("waiting key press")
+	time.sleep(20)
+	imgpro()
+def updatedata(arr):
+	strin = str(arr)
+	url = "http://localhost/record?arr=" + strin
+	r = requests.get(url)
+	print(r.content)
+def imgpro():
+	print("Camera initialising")
+	cam = cv2.VideoCapture(0)
+	font = cv2.FONT_HERSHEY_SIMPLEX
+	for x in range(0,60):
+		print(x)
+		ret, im =cam.read()
+		if(ret == True):			
+			print(str(attendarr))
+			gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+			faces=faceCascade.detectMultiScale(gray, 1.2,5)
+			for(x,y,w,h) in faces:
+				cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
+				Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
+				if(conf<50):
+					attendarr[Id]=1
+					if(Id==1):
+						Id="1"
+					elif(Id==2):
+						Id="2"
+					elif(Id==3):
+						Id="3"
+					elif(Id==4):
+						Id="4"
+					elif(Id==5):
+						Id="5"
+					elif(Id==6):
+						Id="6"
+					elif(Id==7):
+						Id="7"
+					elif(Id==8):
+						Id="8"
+					elif(Id==9):
+						Id="9"
+					elif(Id==10):
+						Id="10"
+				else:
+					Id="Unknown"
+				#cv2.cv.PutText(cv2.cv.fromarray(im),str(Id), (x,y+h),font, 255)
+				cv2.putText(im,str(Id), (x,y+h),font,1,(255, 255,255),2, cv2.LINE_AA)
+			cv2.imshow('im',im)
+			if cv2.waitKey(10) & 0xFF==ord('q'):
+				quit()
+		else:
+			print("camera not detected")
+			time.sleep(3)
+			imgpro()
+	cam.release()
+	cv2.destroyAllWindows()
+	updatedata(attendarr)
+	keydetect()
+	
+keydetect()
